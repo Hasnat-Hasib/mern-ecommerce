3d7cf4d064ec1e12cc0dcd7221b838a8488ecb0a
Hasnat <hasib8nh@gmail.com>
2020-11-01 08:16:38 +0600

    Connecting to Database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 // import routes
-const userRoutes = require("./routes/user");
+//const userRoutes = require("./routes/user");
 
 // app
 const app = express();
@@ -11,15 +11,16 @@ const app = express();
 mongoose
     .connect(process.env.DATABASE, {
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useUnifiedTopology: true
     })
     .then(() => console.log("DB Connected"));
 
 // routes middleware
-app.use("/api", userRoutes);
+//app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+        console.log(`Server is running on port ${port}`);
 });
