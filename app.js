0c4fae1fe35c4b7ce79e6c39594d022f37aa6167
Hasnat <hasib8nh@gmail.com>
2020-11-02 09:57:13 +0600

    User SignUp

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -16,11 +19,16 @@ mongoose
     })
     .then(() => console.log("DB Connected"));
 
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 // routes middleware
 app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
