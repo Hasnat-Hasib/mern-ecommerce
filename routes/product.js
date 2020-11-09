da54365dee0a88a63fcfdb6ad0b0786b9e5cc2d0
Hasnat <hasib8nh@gmail.com>
2020-11-09 08:58:21 +0600

    Update Product

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove } = require("../controllers/product");
+const { create, productById, read, remove, update } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -14,6 +14,15 @@ router.delete(
     isAdmin,
     remove
 );
+
+router.put(
+    "/product/:productId/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update
+);
+
 router.param("userId", userById);
 router.param("productId", productById);
 
